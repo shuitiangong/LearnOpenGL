27410844270b8de5eee35e1b28329a6ef77e0c87
shuitiangong <757153944@qq.com>
2025-02-02 21:28:29 +0800

    transform

---
+++ src/My05_transform/main.cpp
@@ -0,0 +1,244 @@
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<tool/shader.h>
+#include<iostream>
+#include<string>
+#include<math.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include<tool/stb_image.h>
+using namespace std;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, 800, 600);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void CheckShaderCompile(string name, GLuint shader, int statusCode) {
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, statusCode, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+        std::cout << name << " ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+}
+
+std::string Shader::dirName;
+
+int main(int argc, char *argv[]) {
+    Shader::dirName = argv[1];
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //创建窗口对象
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Faile to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    //注册窗口尺寸变化监听
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // 顶点数据
+    float vertices[] = {
+        //---- 位置 ----  ---- 颜色 ----     - 纹理坐标 -
+        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,  // 右上
+        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // 右下
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0, 0.0f, // 左下
+        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // 左上
+    };
+
+    // 索引数据
+    unsigned int indices[] = {
+        0, 1, 3, // 三角形一
+        1, 2, 3  // 三角形二
+    };
+
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    //指定OpenGL该如何解释顶点数据，即一个顶点数据段该如何解释
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6*sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    glBindVertexArray(0);
+
+    //创建2D纹理对象
+    unsigned int texture[2];
+    glGenTextures(2, texture);
+    int width, height, nrChannels;
+    unsigned char* data = nullptr;
+    stbi_set_flip_vertically_on_load(true);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture[0]);
+    //设置环绕、过滤方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    //加载纹理
+    data = stbi_load("./static/texture/container.jpg", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        stbi_image_free(data);
+    }
+    else {
+        cout << "load texture faild!" << endl;
+    }
+
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture[1]);
+    //设置环绕、过滤方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    //加载纹理
+    data = stbi_load("./static/texture/awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+        stbi_image_free(data);
+    }
+    else {
+        cout << "load texture faild!" << endl;
+    }
+
+    
+    Shader shader("./shader/vertex.glsl", "./shader/fragment.glsl");
+
+    shader.use();
+    shader.setInt("texture1", 0);
+    shader.setInt("texture2", 1);
+    
+    unsigned int transformLoc = glGetUniformLocation(shader.ID, "transform");
+
+
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        shader.use();
+
+        glm::mat4 trans = glm::mat4(1.0f);
+        //radians把角度转换成弧度
+        //绕z轴旋转
+        //trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0, 0, 1));
+        trans = glm::translate(trans, glm::vec3(0.5, -0.5, 0));
+        trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
+        trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0, 0, 1));
+        unsigned int transformLoc = glGetUniformLocation(shader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        trans = glm::mat4(1.0f);
+        //radians把角度转换成弧度
+        //绕z轴旋转
+        //trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0, 0, 1));
+        trans = glm::translate(trans, glm::vec3(-0.5, 0.5, 0));
+        float scaleValue = (sin(glfwGetTime())+1.0)/2.0;
+        trans = glm::scale(trans, glm::vec3(scaleValue, scaleValue, 1));
+        trans = glm::rotate(trans, -(float)glfwGetTime(), glm::vec3(0, 0, 1));
+        transformLoc = glGetUniformLocation(shader.ID, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }    
+
+    // while (!glfwWindowShouldClose(window))
+    // {
+    //     // input
+    //     // -----
+    //     processInput(window);
+
+    //     // render
+    //     // ------
+    //     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    //     glClear(GL_COLOR_BUFFER_BIT);
+
+    //     // bind textures on corresponding texture units
+    //     glActiveTexture(GL_TEXTURE0);
+    //     glBindTexture(GL_TEXTURE_2D, texture[0]);
+    //     glActiveTexture(GL_TEXTURE1);
+    //     glBindTexture(GL_TEXTURE_2D, texture[1]);
+
+
+    //     glm::mat4 transform = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+    //     // first container
+    //     // ---------------
+    //     transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+    //     transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+    //     // get their uniform location and set matrix (using glm::value_ptr)
+    //     unsigned int transformLoc = glGetUniformLocation(shader.ID, "transform");
+    //     glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+    //     // with the uniform matrix set, draw the first container
+    //     glBindVertexArray(VAO);
+    //     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    //     // second transformation
+    //     // ---------------------
+    //     transform = glm::mat4(1.0f); // reset it to identity matrix
+    //     transform = glm::translate(transform, glm::vec3(-0.5f, 0.5f, 0.0f));
+    //     float scaleAmount = static_cast<float>(sin(glfwGetTime()));
+    //     transform = glm::scale(transform, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
+    //     glUniformMatrix4fv(transformLoc, 1, GL_FALSE, &transform[0][0]); // this time take the matrix value array's first element as its memory pointer value
+
+    //     // now with the uniform matrix being replaced with new transformations, draw it again.
+    //     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    //     // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+    //     // -------------------------------------------------------------------------------
+    //     glfwSwapBuffers(window);
+    //     glfwPollEvents();
+    // }
+
+    glDeleteBuffers(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    
+    glfwTerminate();
+    return 0;
+}
