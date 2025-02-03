987afb3453281548f814a9b9721c7513e8ce1aa6
shuitiangong <757153944@qq.com>
2025-02-03 18:47:50 +0800

    完善shader类

--- include/tool/shader.h
+++ include/tool/shader.h
@@ -178,6 +178,10 @@ public:
         glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
     }
 
+    ~Shader() {
+        glDeleteProgram(ID);
+    }
+
 private:
     // utility function for checking shader compilation/linking errors.
     // ------------------------------------------------------------------------
